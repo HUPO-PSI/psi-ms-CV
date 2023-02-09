d07301f1ffcd9fa2643e3d58a363b528f6a49250
Joshua Klein <mobiusklein@gmail.com>
2023-02-08 19:12:02 -0500

    Add cross-linking updates

--- scripts/find_next_available_number.py
+++ scripts/find_next_available_number.py
@@ -9,27 +9,32 @@ def collect_gaps(stream, min_value=1000300):
     last_seen = dict()
     gaps = []
 
-    for line in stream:
-        line = line.strip()
-        is_id = id_pattern.match(line)
-        if is_id:
-            cv, acc = is_id.groups()
-            acc = int(acc)
-            try:
-                last_in_cv: int = last_seen[cv]
-            except KeyError:
+    try:
+        for i, line in enumerate(stream):
+            line = line.strip()
+            is_id = id_pattern.match(line)
+            if is_id:
+                cv, acc = is_id.groups()
+                acc = int(acc)
+                try:
+                    last_in_cv: int = last_seen[cv]
+                except KeyError:
+                    last_seen[cv] = acc
+                    continue
+                diff = acc - last_in_cv
+                if diff > 1 and acc > min_value:
+                    for i in range(1, diff):
+                        gaps.append((cv, last_in_cv + i))
+                    sys.stderr.write(f"Found gap {gaps[-diff + 1]} to {gaps[-1]}\n")
                 last_seen[cv] = acc
-                continue
-            diff = acc - last_in_cv
-            if diff > 1 and acc > min_value:
-                for i in range(1, diff):
-                    gaps.append((cv, last_in_cv + i))
-                sys.stderr.write(f"Found gap {gaps[-diff + 1]} to {gaps[-1]}\n")
-            last_seen[cv] = acc
-    return gaps, last_seen
+        return gaps, last_seen
+    except UnicodeDecodeError:
+        print(f"Failed to decode line {i}")
+        raise
 
 
 if __name__ == "__main__":
     path = sys.argv[1]
-    with open(path, 'rt') as stream:
+    with open(path, 'rt', encoding='utf8') as stream:
         gaps, last_seen = collect_gaps(stream)
+
